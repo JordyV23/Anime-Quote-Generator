66d2b9e8311ef1d23685c8c48d47a48142520808
JordyV23 <jordyvargas0123@gmail.com>
2023-01-20 19:01:09 -0600

    Inicio de proyecto y Creacion de componentes Quote y Header

---
+++ anime-quote-generator/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <Header title="The anime quoter"/>
+    <Quote />
+  </div>
+</template>
+
+<script>
+import Header from './components/HeaderComponent.vue';
+import Quote from './components/QuoteComp.vue'
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Quote
+  },
+};
+</script>
+
+<style lang="scss">
+:root{
+    --primary: #D81E5B;
+    --secondary: #8A4FFF;
+    --tertiary:#32CBFF;
+    --dark: #131A26;
+    --light: #EEE;
+    --grey: #848484;
+}
+
+ *{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+ }
+</style>
